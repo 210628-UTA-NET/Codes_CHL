50713aff09af7eabb43019535c04ad8fa939ace1
Chris Lee <chris.lee@revature.net>
2021-07-10 05:54:03 -0700

    commit

---
+++ StoreReview/Store.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StoreReview
+{
+    public class Store
+    {
+        private string _city; ///Backing Field.
+        public Store()
+        {
+            
+        }
+        public string Name {get; set;}
+        public string City{ 
+            
+           get
+           {
+               return _city;
+           }
+            
+           set
+           {
+                if (!Regex.IsMatch(value,"@^[A-Za-z .]+$"))
+                {
+                    throw new Exception("City can only hold letters. Try again.");
+                }
+            }
+            
+        }
+        
+        public string State {get; set;}
+        
+    }
+}
