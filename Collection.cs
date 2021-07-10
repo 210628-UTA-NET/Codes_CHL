1b67908e171f4605b4f3bc2ef2f71d99ea6e5148
Chris Lee <chris.lee@revature.net>
2021-07-10 04:33:44 -0700

    collections started

---
+++ Collection.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Test_Cases
+{
+    public class Collection
+    {
+        //generic field
+
+        List<string> strings = new List<string>();
+    
+            public static void CollectionMain()
+        {
+            Console.WriteLine("===Collection Demo===");
+            Console.WriteLine("===List Demo===");
+        }
+
+    }
+
+}
